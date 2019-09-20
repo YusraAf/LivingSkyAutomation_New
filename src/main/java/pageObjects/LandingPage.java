701a95f78d6b757d1a01136dd4e0ad76f12488b5
Chetankumar Patel <chetankumarpatel@ChetankumarsMBP.lan>
2019-09-20 09:28:55 -0600

    Changes in LandingPage and LandingPageTest

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -31,4 +31,114 @@ public class LandingPage extends PageBase{
 	@FindBy(xpath="//div[2]//nav[1]//ul[1]")
 	@CacheLookup
 	public WebElement navBar_HeaderContent2;*/
+	@FindBy(xpath="//a[@name='Try it for free']")
+	@CacheLookup
+	public WebElement navBar_HeaderContent3;
+
+	@FindBy(xpath="//div[@class='main']/div[2]/div/div/div/a[1]")
+	@CacheLookup
+	public WebElement btn_Tryforfree;
+	
+	@FindBy(xpath="//div[@class='main']/div[2]/div/div/div/a[2]")
+	@CacheLookup
+	public WebElement btn_LearnMore;
+	
+	@FindBy(xpath="//div[@class='cta-link']/button")
+	@CacheLookup
+	public WebElement btn_TryLiveDemo;
+	
+	@FindBy(xpath="//h2[contains(text(),'Writing - the way it should be.')]")
+	@CacheLookup
+	public WebElement text_Writing;
+	
+	@FindBy(xpath="//div[@class='benefits']/div[1]/img")
+	@CacheLookup
+	public WebElement img_NoLimitions;
+	@FindBy(xpath="//div[@class='benefits']/div[2]/img")
+	@CacheLookup
+	public WebElement img_Cloudbased;
+	@FindBy(xpath="//div[@class='benefits']/div[3]/img")
+	@CacheLookup
+	public WebElement img_Mindmaps;
+	@FindBy(xpath="//div[@class='benefits']/div[4]/img")
+	@CacheLookup
+	public WebElement img_RefManager;
+	@FindBy(xpath="//div[@class='benefits']/div[5]/img")
+	@CacheLookup
+	public WebElement img_AutoFormat;
+	@FindBy(xpath="//div[@class='benefits']/div[6]/img")
+	@CacheLookup
+	public WebElement img_ThemeDesigner;
+	@FindBy(xpath="//div[@class='benefits']/div[7]/img")
+	@CacheLookup
+	public WebElement img_ArtIntelligence;
+	@FindBy(xpath="//div[@class='benefits']/div[8]/img")
+	@CacheLookup
+	public WebElement img_MultipleDevices;
+	@FindBy(xpath="//h4[contains(text(),'No limitations')]")
+	@CacheLookup
+	public WebElement lbl_NoLimitions;
+	@FindBy(xpath="//h4[contains(text(),'Cloud-based')]")
+	@CacheLookup
+	public WebElement lbl_Cloudbased;
+	@FindBy(xpath="//h4[contains(text(),'Mindmaps')]")
+	@CacheLookup
+	public WebElement lbl_Mindmaps;
+	@FindBy(xpath="//h4[contains(text(),'Reference manager')]")
+	@CacheLookup
+	public WebElement lbl_RefManager;
+	@FindBy(xpath="//h4[contains(text(),'Auto-formatting')]")
+	@CacheLookup
+	public WebElement lbl_AutoFormat;
+	@FindBy(xpath="//h4[contains(text(),'Theme designer')]")
+	@CacheLookup
+	public WebElement lbl_ThemeDesigner;
+	@FindBy(xpath="//h4[contains(text(),'Artificial intelligence')]")
+	@CacheLookup
+	public WebElement lbl_ArtIntelligence;
+	@FindBy(xpath="//h4[contains(text(),'Multiple Devices')]")
+	@CacheLookup
+	public WebElement lbl_MultipleDevices;
+	@FindBy(xpath="//div[@class='benefits']/div[1]/p")
+	@CacheLookup
+	public WebElement text_NoLimitions;
+	@FindBy(xpath="//div[@class='benefits']/div[2]/p")
+	@CacheLookup
+	public WebElement text_Cloudbased;
+	@FindBy(xpath="//div[@class='benefits']/div[3]/p")
+	@CacheLookup
+	public WebElement text_Mindmaps;
+	@FindBy(xpath="//div[@class='benefits']/div[4]/p")
+	@CacheLookup
+	public WebElement text_RefManager;
+	@FindBy(xpath="//div[@class='benefits']/div[5]/p")
+	@CacheLookup
+	public WebElement text_AutoFormat;
+	@FindBy(xpath="//div[@class='benefits']/div[6]/p")
+	@CacheLookup
+	public WebElement text_ThemeDesigner;
+	@FindBy(xpath="//div[@class='benefits']/div[7]/p")
+	@CacheLookup
+	public WebElement text_ArtIntelligence;
+	@FindBy(xpath="//div[@class='benefits']/div[8]/p")
+	@CacheLookup
+	public WebElement text_MultipleDevices;
+	@FindBy(xpath="//div[@class='stacked-text']/div[1]/div[2]")
+	@CacheLookup
+	public WebElement text_Plan;
+	@FindBy(xpath="//div[contains(text(),'Write')]")
+	@CacheLookup
+	public WebElement text_Write;
+	@FindBy(xpath="//div[contains(text(),'Publish')]")
+	@CacheLookup
+	public WebElement text_Publish;
+	@FindBy(xpath="//div[@class='stacked-text']/div[1]/div[1]/img")
+	@CacheLookup
+	public WebElement img_Plan;
+	@FindBy(xpath="//div[@class='stacked-text']/div[2]/div[2]/img")
+	@CacheLookup
+	public WebElement img_Write;
+	@FindBy(xpath="//div[@class='stacked-text']/div[1]/div[2]/img")
+	@CacheLookup
+	public WebElement img_Publish;
 }
--- src/test/java/LST/Lst/LandingPageTest.java
+++ src/test/java/LST/Lst/LandingPageTest.java
@@ -1,5 +1,6 @@
 package LST.Lst;
 
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
@@ -64,6 +65,99 @@ private LoginPage login;
 	
 	
 	@Test(priority = 2)
+	
+	public void verifyLandingPageAttributes() throws InterruptedException {
+		
+		 LandingPage l= new LandingPage(driver);
+				 //PageFactory.initElements(driver, LandingPage.class);
+		 l.initElement();
+		 
+		 LandingPage log =new LandingPage(l.getDriver());
+				 //PageFactory.initElements(driver, LoginPage.class);
+		 log.initElement();
+		 
+		 AssertJUnit.assertTrue(log.btn_Tryforfree.isDisplayed());
+		 logger.info("Try for free button is displayed.");
+		 AssertJUnit.assertTrue(log.btn_LearnMore.isDisplayed());
+		 logger.info("Try Learn More button is displayed.");
+		 
+		  WebElement element = driver.findElement(By.xpath("//a[@name='Try for free']"));
+		  
+		  JavascriptExecutor jse = (JavascriptExecutor)driver;
+		  jse.executeScript("arguments[0].scrollIntoView();", element);
+		  
+		  AssertJUnit.assertTrue(log.text_Plan.isDisplayed());
+		  logger.info("Plan text is displayed."); Thread.sleep(5000);
+		  AssertJUnit.assertTrue(log.text_Write.isDisplayed());
+		  logger.info("Write text is displayed.");
+		  AssertJUnit.assertTrue(log.text_Publish.isDisplayed());
+		  logger.info("Publish text is displayed.");
+		  
+		  // Verify All Images display on landing page  
+		  AssertJUnit.assertTrue(log.img_NoLimitions.isDisplayed());
+		  logger.info("No Limitions image is displayed.");
+		  AssertJUnit.assertTrue(log.img_Cloudbased.isDisplayed());
+		  logger.info("Cloud based image is displayed.");
+		  AssertJUnit.assertTrue(log.img_Mindmaps.isDisplayed());
+		  logger.info("Mind maps image is displayed.");
+		  AssertJUnit.assertTrue(log.img_RefManager.isDisplayed());
+		  logger.info("Ref Manager image is displayed.");
+		  AssertJUnit.assertTrue(log.img_AutoFormat.isDisplayed());
+		  logger.info("Auto Format image is displayed.");
+		  AssertJUnit.assertTrue(log.img_ThemeDesigner.isDisplayed());
+		  logger.info("Theme Designer image is displayed.");
+		  AssertJUnit.assertTrue(log.img_ArtIntelligence.isDisplayed());
+		  logger.info("Art Intelligence image is displayed.");
+		  AssertJUnit.assertTrue(log.img_MultipleDevices.isDisplayed());
+		  logger.info("Multiple Devices image is displayed.");
+		  
+		// Verify All Labels display on landing page
+		  
+		  AssertJUnit.assertTrue(log.lbl_NoLimitions.isDisplayed());
+		  logger.info("No Limitions label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_Cloudbased.isDisplayed());
+		  logger.info("Cloud based label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_Mindmaps.isDisplayed());
+		  logger.info("Mind maps label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_RefManager.isDisplayed());
+		  logger.info("Ref Manager label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_AutoFormat.isDisplayed());
+		  logger.info("Auto Format label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_ThemeDesigner.isDisplayed());
+		  logger.info("Theme Designer label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_ArtIntelligence.isDisplayed());
+		  logger.info("Art Intelligence label is displayed.");
+		  AssertJUnit.assertTrue(log.lbl_MultipleDevices.isDisplayed());
+		  logger.info("Multiple Devices label is displayed.");
+		 
+		// Verify All Texts display on landing page
+		 
+		  AssertJUnit.assertTrue(log.text_NoLimitions.isDisplayed());
+		  logger.info("No Limitions text is displayed.");
+		  AssertJUnit.assertTrue(log.text_Cloudbased.isDisplayed());
+		  logger.info("Cloud based text is displayed.");
+		  AssertJUnit.assertTrue(log.text_Mindmaps.isDisplayed());
+		  logger.info("Mind maps text is displayed.");
+		  AssertJUnit.assertTrue(log.text_RefManager.isDisplayed());
+		  logger.info("Ref Manager text is displayed.");
+		  AssertJUnit.assertTrue(log.text_AutoFormat.isDisplayed());
+		  logger.info("Auto Format text is displayed.");
+		  AssertJUnit.assertTrue(log.text_ThemeDesigner.isDisplayed());
+		  logger.info("Theme Designer text is displayed.");
+		  AssertJUnit.assertTrue(log.text_ArtIntelligence.isDisplayed());
+		  logger.info("Art ntelligence text is displayed.");
+		  AssertJUnit.assertTrue(log.text_MultipleDevices.isDisplayed());
+		  logger.info("Multiple Devices text is displayed.");
+		 
+		  
+		  
+		  AssertJUnit.assertTrue(log.text_Writing.isDisplayed());
+		  logger.info("Writing - the way it should be text is displayed.");
+		  AssertJUnit.assertTrue(log.btn_TryLiveDemo.isDisplayed());
+		  logger.info("Try Live Demo button is displayed.");
+		 	 
+	}
+@Test(priority=3)
 	public void clickOnLogin() throws InterruptedException {
 		 
 		//Need to add initlement for page factory
@@ -75,9 +169,8 @@ private LoginPage login;
 		 login= new LoginPage(landing.driver);
 		 login.verifyPageUrl();
 		 Thread.sleep(500);
+		 
 	}
-	
-	
 	
 	@AfterClass
 	public  void closeBrowser() {
