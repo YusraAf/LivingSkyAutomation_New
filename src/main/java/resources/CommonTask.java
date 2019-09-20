3a4ee43abf40d3527b0a9af369acf91e81e46b37
Chetankumar Patel <chetankumarpatel@ChetankumarsMBP.lan>
2019-09-20 12:43:40 -0600

    Changes in SignUP anmd SignupTest

--- src/main/java/resources/CommonTask.java
+++ src/main/java/resources/CommonTask.java
@@ -3,7 +3,10 @@ package resources;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class CommonTask {
 	
--- src/test/java/LST/Lst/SignUpTest.java
+++ src/test/java/LST/Lst/SignUpTest.java
@@ -1,11 +1,15 @@
 package LST.Lst;
 
+import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
@@ -28,9 +32,11 @@ import pageObjects.LoginPage;
 import pageObjects.SignUP;
 
 public class SignUpTest extends TestBase{
-	
+
+public static Logger logger = LogManager.getLogger(TestBase.class.getName());
 private LandingPage landing;	
 private SignUP signup;
+
 private CommonTestForAllPages common;
 	
 @BeforeClass
@@ -41,56 +47,74 @@ private CommonTestForAllPages common;
 	
 	}
 	
-
-@Test(priority=1)
-public void verifyHeadNavForAllPages() {
-	signup = new SignUP(driver);
-	signup.initElement();
-	common = new CommonTestForAllPages();
-	common.verifyNavBarHeaderContent(driver);
 	
-}
+	  @Test(priority=1) public void verifyHeadNavForAllPages() { 
+		  signup = new SignUP(driver); 
+		  signup.initElement(); 
+		  common = new CommonTestForAllPages();
+	  common.verifyNavBarHeaderContent(driver);
+	  
+	  }
+	  
+	  @Test(priority=2)
+	  public void checkSignupAttributes() throws InterruptedException {
+	 	// basePageNavigation();
+	 	 driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS); 
+	 	 common = new CommonTestForAllPages();
+	 	 landing= new LandingPage(driver);
+	 	 landing.initElement();
+	 	 signup = new SignUP(landing.getDriver());
+	 	 signup.initElement();
+	 	 
+	 	 landing.txt_signup.click();
+	 	 
+	 	 SignUP log =new SignUP(signup.getDriver());
+	 	 log.initElement();
+	 	 
+	 	 AssertJUnit.assertTrue(log.btn_Facebook.isDisplayed());
+	 	 logger.info("Sign up with Facebook button is displayed.");
+	 	 AssertJUnit.assertTrue(log.btn_Google.isDisplayed());
+	 	 logger.info("Sign up with Google button is displayed.");
+	 	 AssertJUnit.assertTrue(log.btn_TermsPolicy.isDisplayed());
+	 	 logger.info("Terms and Policy button is displayed.");
+	 	 AssertJUnit.assertTrue(log.btn_WriteWaySignUP.isDisplayed());
+	 	 logger.info("I want to receive updates about WriteWay button is displayed.");
+	 	 
+	  }
+	  
+	  
+	  @Test(priority=3) public void checkSignupA() throws InterruptedException { 
+		  //basePageNavigation(); 
+	  driver.manage().timeouts().implicitlyWait(50,
+	  TimeUnit.SECONDS); common = new CommonTestForAllPages(); landing= new
+	  LandingPage(driver); landing.initElement(); signup = new
+	  SignUP(landing.getDriver()); signup.initElement();
+	  
+	  landing.txt_signup.click();
+	  
+	  signup.verifyPageUrl();
+	  System.out.println("Random Number: "+common.randomNumber());
+	  signup.txt_userName.click();
+	  signup.txt_userName.sendKeys("test"+common.randomNumber()+"@yopmail.com");
+	  
+	  signup.txt_password.click();
+	  
+	  signup.txt_password.sendKeys("asdF1234");
+	  
+	  signup.txt_confirmPassword.click();
+	  signup.txt_confirmPassword.sendKeys("asdF1234");
+	  signup.radio_termsandCond.click(); signup.radio_updates.click();
+	  signup.btn_signUp.click(); signup.txt_capcha.click();
+	  signup.txt_capcha.sendKeys("ssssfdg"); Thread.sleep(200);
+	  signup.btn_capcha.click(); Thread.sleep(20000);
+	  //driver.findElement(By.cssSelector(".content-canvas__container")).click();
+	  
+	  
+	  System.out.println("+++++++++++++++++++++++IN Signup+++++++++++++++++");
+	  
+	  }
+	 
  
-	
- @Test(priority=2)
- public void checkSignupA() throws InterruptedException {
-	// basePageNavigation();
-	 driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS); 
-	 common = new CommonTestForAllPages();
-	 landing= new LandingPage(driver);
-	 landing.initElement();
-	 signup = new SignUP(landing.getDriver());
-	 signup.initElement();
-	 
-	 landing.txt_signup.click();
-		 
-	 signup.verifyPageUrl();
-	 System.out.println("Random Number: "+common.randomNumber());
-	 signup.txt_userName.click();
-	 signup.txt_userName.sendKeys("test"+common.randomNumber()+"@yopmail.com");
-	    
-	 signup.txt_password.click();
-	    
-	 signup.txt_password.sendKeys("asdF1234");
-	   	
-	 signup.txt_confirmPassword.click();
-	 signup.txt_confirmPassword.sendKeys("asdF1234");
-	 signup.radio_termsandCond.click();
-	 signup.radio_updates.click();
-	 signup.btn_signUp.click();
-	 signup.txt_capcha.click();
-	 signup.txt_capcha.sendKeys("ssssfdg");
-	    Thread.sleep(200); 
-	    signup.btn_capcha.click();
-	    Thread.sleep(20000); 
-	    //driver.findElement(By.cssSelector(".content-canvas__container")).click();
-	 
-	 
-	 System.out.println("+++++++++++++++++++++++IN Signup+++++++++++++++++");
-			
- }
-
-
 @AfterClass
 public  void closeBrowser() {
 	System.out.println("Closing Signup page Test");
