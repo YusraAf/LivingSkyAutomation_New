c36959b1572aca1e97d11e21073f5892955240de
Chetankumar Patel <chetankumarpatel@ChetankumarsMBP.lan>
2019-09-25 10:59:08 -0600

    New updated Changes in log4j2 file

--- src/test/java/LST/Lst/LandingPageTest.java
+++ src/test/java/LST/Lst/LandingPageTest.java
@@ -222,7 +222,7 @@ private LoginPage login;
 		 AssertJUnit.assertTrue(footer.youtubeImg_Footer.isDisplayed());
 		 logger.info("you Tube Image link is displayed on the footer of Landing Page.");	 
 	
-}
+} 
 @Test(priority=4)
 	public void clickOnLogin() throws InterruptedException {
 		 
@@ -233,7 +233,8 @@ private LoginPage login;
 		logger.info("Click on Login page.");
 		 Thread.sleep(500);
 		 login= new LoginPage(landing.driver);
-		 login.verifyPageUrl();
+		 
+		 System.out.println(login.verifyPageUrl());
 		 Thread.sleep(500);
 		 
 	}
