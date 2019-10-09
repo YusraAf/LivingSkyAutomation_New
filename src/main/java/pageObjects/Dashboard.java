8267a0cc1f4544e3ed4243dbf5cbe9eee887416a
Chetankumar Patel <chetankumarpatel@ChetankumarsMBP.lan>
2019-10-09 16:08:38 -0600

    changes in dashboard dashboardtest extentreport and testng

--- src/main/java/pageObjects/Dashboard.java
+++ src/main/java/pageObjects/Dashboard.java
@@ -31,4 +31,53 @@ public WebElement link_collections;
 @FindBy(xpath=("//button[@class='logout']"))
 @CacheLookup
 public WebElement btn_logout;
+
+@FindBy(xpath= "//div[contains(text(),'Favourites')]")
+@CacheLookup
+public WebElement link_favourites;
+
+@FindBy(xpath= "//div[contains(text(),'Trash')]")
+@CacheLookup
+public WebElement link_trash;
+
+@FindBy(xpath= "//span[@class='menu-btn-text delete-btn-text']")
+@CacheLookup
+public WebElement link_EmptyTrash;
+
+@FindBy(xpath= "//div[contains(text(),'My Account')]")
+@CacheLookup
+public WebElement link_myAccount;
+
+@FindBy(xpath= "//div[contains(text(),'Settings')]")
+@CacheLookup
+public WebElement link_settings;
+
+@FindBy(xpath= "//div[contains(text(),'Help')]")
+@CacheLookup
+public WebElement link_help;
+
+@FindBy(xpath= "//span[@class='menu-btn-text']")
+@CacheLookup
+public WebElement btn_newProject;
+
+@FindBy(xpath= "//span[contains(text(),'Start Project')]")
+@CacheLookup
+public WebElement btn_startProject;
+
+@FindBy(xpath= "//span[contains(text(),'Add a photo')]")
+@CacheLookup
+public WebElement btn_addPhoto;
+
+@FindBy(xpath= "//span[contains(text(),'Add description')]")
+@CacheLookup
+public WebElement btn_addDescription;
+
+@FindBy(xpath= "//input[@id='input-name']")
+@CacheLookup
+public WebElement nav_startTypingProjectName;
+
+@FindBy(xpath= "//button[@id='button-k6usgnju']")
+@CacheLookup
+public WebElement dropDownbtn_sortBylastModified;
+
 }
---
+++ src/test/java/LST/Lst/DashboardTest.java
@@ -0,0 +1,95 @@
+package LST.Lst;
+
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import LST.core.TestBase;
+import pageObjects.Dashboard;
+import pageObjects.LandingPage;
+import pageObjects.LoginPage;
+import resources.Browser;
+
+public class DashboardTest extends TestBase {
+	
+	public static Logger logger = LogManager.getLogger(TestBase.class.getName());
+	private LandingPage land;
+	private LoginPage log;
+	private Dashboard das;
+	
+	@BeforeClass
+	public void openBrowser() throws IOException, InterruptedException {	
+		driver = Browser.getInstance();
+		driver.get(baseUrl);
+	}	
+
+	@Test(priority=1) 
+	public void verifyDashboardPageAttributes() throws IOException, InterruptedException {
+		
+		land= new LandingPage(driver);
+		land.initElement(); 
+		land.txt_login.click();
+		 
+		log =new LoginPage(land.getDriver());
+		log.initElement();
+		  
+		log.txt_userName.sendKeys("niti12@livingskytech.com"); 
+		log.txt_password.sendKeys("asdF1234");
+		log.btn_login.click();
+		Thread.sleep(200);
+	 
+		das = new Dashboard(log.getDriver());
+		das.initElement();
+		das.verifyPageUrl();
+		
+		AssertJUnit.assertTrue(das.link_projects.isDisplayed());
+		logger.info("Projects is displayed on dashboard.");
+		
+		WebElement element =das.link_projects; 
+		Actions builder = new Actions(driver); 
+		builder.moveToElement(element).build().perform(); 
+		  
+		AssertJUnit.assertTrue(das.link_favourites.isDisplayed());
+		logger.info("Favourites is displayed on dashboard.");
+		  
+		AssertJUnit.assertTrue(das.link_trash.isDisplayed());
+		logger.info("Trash is displayed on dashboard.");
+		
+		AssertJUnit.assertTrue(das.link_collections.isDisplayed());
+		logger.info("Collection is displayed on dashboard.");
+		
+		AssertJUnit.assertTrue(das.link_myAccount.isDisplayed());
+		logger.info("My Account is displayed on dashboard.");
+		
+		AssertJUnit.assertTrue(das.link_settings.isDisplayed());
+		logger.info("Settings is displayed on dashboard.");
+		
+		das.btn_newProject.click();
+		
+		AssertJUnit.assertTrue(das.btn_startProject.isDisplayed());
+		logger.info("Start Project button is displayed on dashboard.");
+		
+		AssertJUnit.assertTrue(das.btn_addDescription.isDisplayed());
+		logger.info("Add Description button is displayed on dashboard.");
+		
+		AssertJUnit.assertTrue(das.btn_addPhoto.isDisplayed());
+		logger.info("Add Photo button is displayed on dashboard.");
+	
+		AssertJUnit.assertTrue(das.nav_startTypingProjectName.isDisplayed());
+		logger.info("Start typing your project name is displayed on dashboard.");
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		System.out.println("Closing Login page Test");
+		Browser.close();
+	}
+
+
+}
