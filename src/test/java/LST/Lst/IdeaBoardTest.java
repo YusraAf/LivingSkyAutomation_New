9f97a51ff37cbc28dddd1a6b91263c3161f95d19
Niti Jabin <nitijabin@Nitis-MBP.lan>
2019-12-11 11:24:42 -0600

    added new

--- src/test/java/LST/Lst/IdeaBoardTest.java
+++ src/test/java/LST/Lst/IdeaBoardTest.java
@@ -26,7 +26,7 @@ public class IdeaBoardTest extends TestBase {
 	public static Logger logger = LogManager.getLogger(TestBase.class.getName());
 	private LandingPage land;
 	private LoginPage log;
-	private Dashboard das;
+	private Dashboard das = new Dashboard(driver);
 	private Project pro;
 	private Ideaboard idb;
 	private CommonTask com  = new CommonTask(driver);
@@ -54,10 +54,10 @@ public class IdeaBoardTest extends TestBase {
 
 	
 @Test(priority=1) 	
-public void createNewIdea() throws IOException, InterruptedException, Exception {
+public void createNewIdeaWithParagraphAndImageAndScrolling() throws IOException, InterruptedException, Exception {
 	
 	
-	das = new Dashboard(driver);
+	//das = new Dashboard(driver);
 	das.initElement();
 	
 	das.btn_newProject.click();
@@ -83,7 +83,7 @@ public void createNewIdea() throws IOException, InterruptedException, Exception 
 		idb.initElement();
 		
 		idb.btn_newIdea_IdeaBoard.click();
-		
+		Thread.sleep(800);
 		//idb.nav_startTyping_Canvas.sendKeys("About Living Sky Technologies Empowering creators everywhere. What if an idea could exist interrupted, flowing freely from the imagination to the page? What if the research didn’t draw you away from the page, or formatting didn’t drag out the process? Answering these questions has led the Living Sky Technologies team to discover a way to simplify the creativity process by building a platform from which any content creator can be launched. In an era of endless digital distractions, creating content can be burdened by the need to format content. The goal of Living Sky Technologies is to stamp out these distractions and guide the content creator down a clear, productive path. The Living Sky team understands how the process of modern writing can cause distractions and impair the flow of productivity. Our software is able to establish patterns from the seemingly random nature of drafting, formatting, and editing to enhance the writing and content creation process. Vision Our vision is to provide a suite of products and services that are indispensable to content creators across all disciplines. Mission  The mission of Living Sky Technologies is to streamline your workflow by making content creation more efficient.");
 		
 		idb.nav_startTyping_Canvas.sendKeys("About Living Sky Technologies Empowering creators everywhere.\n"+ 
@@ -93,7 +93,8 @@ public void createNewIdea() throws IOException, InterruptedException, Exception 
 				"\n" + "Our vision is to provide a suite of products and services that are indispensable to content creators across all disciplines." + 
 				"\n" + "Mission" + 
 				"\n" + "The mission of Living Sky Technologies is to streamline your workflow by making content creation more efficient.");
-		Thread.sleep(10000);
+		
+		Thread.sleep(2000);
 		com.mouseHoverOnly(idb.objectCreationControlBar_container_Canvas);
 		
 		File file = new File("Images/project_image.jpeg"); 
@@ -114,6 +115,36 @@ public void createNewIdea() throws IOException, InterruptedException, Exception 
 		Thread.sleep(500);
 		com.moveMouseAndClick(pro.btn_first_Project_open_grid);
 		Thread.sleep(2000);
+}
+
+@Test(priority=2) 	
+public void createNewIdeaWithImageObject() throws IOException, InterruptedException, Exception {
+	idb = new Ideaboard(pro.getDriver());
+	idb.initElement();
+	idb.btn_newIdea_IdeaBoard.click();
+	Thread.sleep(800);
+	
+	File file = new File("Images/images2.jpeg"); 
+	String path = file.getAbsolutePath(); 
+	idb.iconCamera_Canvas.sendKeys(path);
+	
+	idb.nav_imageTitle_imageEditor.sendKeys("Add Image testing with paragraph in chrome browser");
+	idb.nav_imageCaption_imageEditor.sendKeys("Image Caption added");
+	idb.nav_imageAttribution_imageEditor.sendKeys("Image Object attribute");
+	Thread.sleep(2000);
+	
+	idb.btn_imageSaveClose_imageEditor.click();
+	Thread.sleep(2000);
+	
+	idb.btn_canvasSaveClose_Canvas.click();
+	Thread.sleep(200);
+	pro = new Project(das.getDriver());
+	pro.initElement();
+	das.initElement();
+	das.link_projects.click();
+	Thread.sleep(500);
+	com.moveMouseAndClick(pro.btn_first_Project_open_grid);
+	Thread.sleep(2000);
 }
 	
 	@AfterClass
